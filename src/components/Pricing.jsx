ddfa1defe44c126c45e10b47db99c0f790207de5
Diego Iacub <105656485+iacubdiego@users.noreply.github.com>
2024-08-03 23:28:32 -0300

    Update Pricing.jsx

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,80 +1,74 @@
 import React from "react";
-
-import botas from "../images/ropa/20170512_115346.jpg";
-import camisa from "../images/ropa/Camisa Beige.jpg";
-import pantalones from "../images/ropa/Pantalon Azul.jpg";
+import botas from "../images/botin.png";
+import camisa from "../images/Elementosdeseguridad.jpg";
+import pantalones from "../images/botin.png";
 
 export default function Pricing() {
   return (
-    <>
-      <div className="flex mx-auto my-8 justify-center gap-6">
-        {/*<!-- Component: Basic Pricing Table --> */}
-        <div className="max-w-sm overflow-hidden bg-white rounded shadow-lg lg:max-md-full text-slate-500 shadow-slate-200">
-          <div className="flex flex-col">
-            <header className="flex flex-col gap-6 p-6 text-slate-400">
-              <h3 className="text-xl font-bold text-slate-700">
-                Botas
-                <span className="block text-sm font-normal text-slate-400">
-                  Ideal para el camino.
-                </span>
-              </h3>
-              <img
-                src={botas}
-                alt="botas"
-                className="object-cover rounded-md w-full h-48 shadow-md"
-              />
-              <button className="inline-flex items-center justify-center w-full h-12 gap-2 px-6 text-sm font-medium tracking-wide text-white transition duration-300 rounded shadow-lg whitespace-nowrap bg-gray-500 shadow-gray-100 hover:bg-gray-600 hover:shadow-md hover:shadow-gray-100 focus:bg-gray-700 focus:shadow-md focus:shadow-gray-100 focus-visible:outline-none">
-                <span>Ver más Productos</span>
-              </button>
-            </header>
-          </div>
+    <div className="flex mx-auto h-[400px] my-8 justify-center gap-6">
+      {/*<!-- Component: Basic Pricing Table --> */}
+      <div className="relative min-w-[300px] max-w-sm overflow-hidden bg-white rounded shadow-lg lg:max-md-full text-slate-500 shadow-slate-200 group">
+        <div
+          className="absolute inset-0 bg-cover bg-center"
+          style={{ backgroundImage: `url(${botas})` }}
+        ></div>
+        <div className="relative flex flex-col justify-center p-6 text-slate-400 h-full bg-white bg-opacity-25 group-hover:bg-opacity-0 transition duration-300">
+          <header className="flex flex-col gap-6">
+            <h3 className="text-4xl font-bold text-white text-slate-700 hidden group-hover:block">
+              Botas
+              <span className="block text-xl text-white font-normal text-slate-400">
+                Buscar profesional con calificación perfecta.
+              </span>
+            </h3>
+            <button className="inline-flex items-center justify-center w-full h-12 gap-2 px-6 text-xl font-medium tracking-wide text-white transition duration-300 rounded shadow-lg whitespace-nowrap bg-gray-500 shadow-gray-100 hover:bg-gray-600 hover:shadow-md hover:shadow-gray-100 focus:bg-gray-700 focus:shadow-md focus:shadow-gray-100 focus-visible:outline-none">
+              <span>Botas</span>
+            </button>
+          </header>
         </div>
-        {/*<!-- End Basic Pricing Table --> */}
-        {/*<!-- Component: Basic Pricing Table --> */}
-        <div className="max-w-sm overflow-hidden bg-white rounded shadow-lg lg:max-md-full text-slate-500 shadow-slate-200">
-          <div className="flex flex-col">
-            <header className="flex flex-col gap-6 p-6 text-slate-400">
-              <h3 className="text-xl font-bold text-slate-700">
-                Camisa
-                <span className="block text-sm font-normal text-slate-400">
-                  Buscar profesional con calificación perfecta.
-                </span>
-              </h3>
-              <img
-                src={camisa}
-                alt="camisa"
-                className="object-cover rounded-md w-full h-48 shadow-md"
-              />
-              <button className="inline-flex items-center justify-center w-full h-12 gap-2 px-6 text-sm font-medium tracking-wide text-white transition duration-300 rounded shadow-lg whitespace-nowrap bg-gray-500 shadow-gray-100 hover:bg-gray-600 hover:shadow-md hover:shadow-gray-100 focus:bg-gray-700 focus:shadow-md focus:shadow-gray-100 focus-visible:outline-none">
-                <span>Ver más Productos</span>
-              </button>
-            </header>
-          </div>
-        </div>
-        {/*<!-- End Basic Pricing Table --> */}
-        {/*<!-- Component: Basic Pricing Table --> */}
-        <div className="max-w-sm overflow-hidden bg-white rounded shadow-lg lg:max-md-full text-slate-500 shadow-slate-200">
-          <div className="flex flex-col">
-            <header className="flex flex-col gap-6 p-6 text-slate-400">
-              <h3 className="text-xl font-bold text-slate-700">
-                Pantalones
-                <span className="block text-sm font-normal text-slate-400">
-                  Buscar profesional con calificación perfecta.
-                </span>
-              </h3>
-              <img
-                src={pantalones}
-                alt="pantalones"
-                className="object-cover rounded-md w-full h-48 shadow-md"
-              />
-              <button className="inline-flex items-center justify-center w-full h-12 gap-2 px-6 text-sm font-medium tracking-wide text-white transition duration-300 rounded shadow-lg whitespace-nowrap bg-gray-500 shadow-gray-100 hover:bg-gray-600 hover:shadow-md hover:shadow-gray-100 focus:bg-gray-700 focus:shadow-md focus:shadow-gray-100 focus-visible:outline-none">
-                <span>Ver más Productos</span>
-              </button>
-            </header>
-          </div>
-        </div>
-        {/*<!-- End Basic Pricing Table --> */}
       </div>
-    </>
+      {/*<!-- End Basic Pricing Table --> */}
+      {/*<!-- Component: Basic Pricing Table --> */}
+      <div className="relative min-w-[300px] max-w-sm overflow-hidden bg-white rounded shadow-lg lg:max-md-full text-slate-500 shadow-slate-200 group">
+        <div
+          className="absolute inset-0 bg-cover bg-center"
+          style={{ backgroundImage: `url(${camisa})` }}
+        ></div>
+        <div className="relative flex flex-col justify-center p-6 text-slate-400 h-full bg-white bg-opacity-25 group-hover:bg-opacity-0 transition duration-300">
+          <header className="flex flex-col gap-6">
+            <h3 className="text-4xl font-bold text-white text-slate-700 hidden group-hover:block">
+              Camisa
+              <span className="block text-xl font-normal text-slate-400">
+                Buscar profesional con calificación perfecta.
+              </span>
+            </h3>
+            <button className="inline-flex items-center justify-center w-full h-12 gap-2 px-6 text-xl font-medium tracking-wide text-white transition duration-300 rounded shadow-lg whitespace-nowrap bg-gray-500 shadow-gray-100 hover:bg-gray-600 hover:shadow-md hover:shadow-gray-100 focus:bg-gray-700 focus:shadow-md focus:shadow-gray-100 focus-visible:outline-none">
+              <span>Camisa</span>
+            </button>
+          </header>
+        </div>
+      </div>
+      {/*<!-- End Basic Pricing Table --> */}
+      {/*<!-- Component: Basic Pricing Table --> */}
+      <div className="relative min-w-[300px] max-w-sm overflow-hidden bg-white rounded shadow-lg lg:max-md-full text-slate-500 shadow-slate-200 group">
+        <div
+          className="absolute inset-0 bg-cover bg-center"
+          style={{ backgroundImage: `url(${pantalones})` }}
+        ></div>
+        <div className="relative flex flex-col justify-center p-6 text-slate-400 h-full bg-white bg-opacity-25 group-hover:bg-opacity-0 transition duration-300">
+          <header className="flex flex-col gap-6">
+            <h3 className="text-4xl font-bold text-slate-700 hidden group-hover:block">
+              Pantalones
+              <span className="block text-xl font-normal text-slate-400">
+                Buscar profesional con calificación perfecta.
+              </span>
+            </h3>
+            <button className="inline-flex items-center justify-center w-full h-12 gap-2 px-6 text-xl font-medium tracking-wide text-white transition duration-300 rounded shadow-lg whitespace-nowrap bg-gray-500 shadow-gray-100 hover:bg-gray-600 hover:shadow-md hover:shadow-gray-100 focus:bg-gray-700 focus:shadow-md focus:shadow-gray-100 focus-visible:outline-none">
+              <span>Pantalones</span>
+            </button>
+          </header>
+        </div>
+      </div>
+      {/*<!-- End Basic Pricing Table --> */}
+    </div>
   );
 }
