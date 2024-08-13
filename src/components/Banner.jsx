14a054788c71f23f94f7232a904309e289e8066d
Diego Iacub <105656485+iacubdiego@users.noreply.github.com>
2024-08-13 18:52:17 -0300

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -11,14 +11,6 @@ const Banner = () => {
         loop
         muted
       />
-      <div className="relative z-10 p-8 text-center text-white mb-8">
-        <h1 className="text-4xl md:text-4xl max-w-[400px] font-bold mb-4">Calidad en Inundementaria</h1>
-        <p className="text-lg md:text-2xl mb-8">Hacemos la diferencia</p>
-        <button className="px-6 py-3 bg-white text-black rounded-md text-lg md:text-xl hover:bg-blue-700 transition duration-300">
-          Inudmentaria
-        </button>
-      </div>
-      <div className="absolute inset-0 bg-black opacity-50"></div>
     </section>
   );
 };
