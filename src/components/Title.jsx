97cb7ff5379025ee56e31b21d0623d696c9e8a8e
Diego Iacub <105656485+iacubdiego@users.noreply.github.com>
2024-08-12 21:09:43 -0300

    Update Title.jsx

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,14 +4,12 @@ export default function Title() {
   return (
     <>
       {/*      <!-- Component: Elevated centered simple base sized testimonial --> */}
-      <div className=" overflow-hidden text-center bg-white bg-opacity-75 backdrop-blur-sm rounded-lg shadow-2xl text-slate-500 shadow-slate-200">
-        <div className="relative p-6 bg-gradient-to-b from-blue-900 to-black">
+      <div className="max-w-xl overflow-hidden text-center bg-white bg-opacity-75 backdrop-blur-sm rounded-lg shadow-2xl text-slate-500 shadow-slate-200">
+        <div className="relative p-6 bg-white">
           <figure className="relative z-10">
-            <blockquote className="p-6 text-xl leading-relaxed text-white">
+            <blockquote className="p-6 text-xl leading-relaxed">
               <p>
-                Descubre la excelencia en cada detalle: ofrecemos una increíble
-                variedad de productos de la más alta calidad, diseñados para
-                satisfacer todas tus necesidades y superar tus expectativas.
+              En CALO nos especializamos en la fabricación y comercialización de indumentaria laboral y elementos de protección personal. (Abastecemos) Nos dirigimos a una amplia gama de clientes, desde pymes y mayoristas hasta particulares, ofreciendo productos de alta calidad que combinan precio y durabilidad, que permitan equipar a los trabajadores de cada sector donde se forja la Industria Nacional.
               </p>
             </blockquote>
             <figcaption className="flex justify-center items-center gap-4 p-6 pt-2 text-xl text-white-500">
