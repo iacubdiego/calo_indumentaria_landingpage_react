73bf0174f0dbdabf20726154fe605ce8d381c4e7
Diego Iacub <105656485+iacubdiego@users.noreply.github.com>
2025-01-09 15:46:02 -0300

    Update Title.jsx

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-
+import { Link } from "wouter";
 export default function Title() {
   return (
     <>
       {/*      <!-- Component: Elevated centered simple base sized testimonial --> */}
-      <div className="max-w-2xl overflow-hidden text-center bg-white bg-opacity-75 backdrop-blur-sm rounded-lg shadow-2xl text-slate-500 shadow-slate-200">
+      <div className="max-w-6xl overflow-hidden text-center bg-white bg-opacity-75 backdrop-blur-sm rounded-lg shadow-2xl text-slate-500 shadow-slate-200">
         <div className="relative p-6 bg-white">
           <figure className="relative z-10">
             <blockquote className="p-6 text-xl leading-relaxed">
@@ -13,10 +13,11 @@ export default function Title() {
               </p>
             </blockquote>
             <figcaption className="flex justify-center items-center gap-4 p-6 pt-2 text-xl text-white-500">
+            <Link href="/Conocenos">
             <button className="px-6 py-3 justify-center bg-white text-black rounded-md  text-lg md:text-xl hover:bg-blue-700 transition duration-300">
-          Contactarse
-        </button>
-
+          Conocenos
+            </button>
+            </Link>
             </figcaption>
           </figure>
         </div>
