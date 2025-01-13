ba4c628d060ab3fce0a0860ee6d23d9154908de3
Diego Iacub <105656485+iacubdiego@users.noreply.github.com>
2025-01-13 11:43:34 -0300

    Update index.js

--- src/pages/Indumentaria/index.js
+++ src/pages/Indumentaria/index.js
@@ -6,22 +6,26 @@ import camisaazulfrente from "../../images/camisa azulino frente.jpg";
 import camisamarino from "../../images/camisa frente marino.jpg";
 import camisabeige from "../../images/camisa beige frente.jpg";
 
+import pantaloncolores from "../../images/pantalon 3 colores.jpg";
+import pantalonbeige from "../../images/pantalon beige.jpg";
+import pantalonazulino from "../../images/pantalon clasico azulino.jpg";
+import pantalonmarino from "../../images/pantalon clasico marino frente.jpg";
+
 export default function Indumentaria() {
   return (
     <>
    <div className="container mx-auto ">
-    
     <div className="relative p-4 grid grid-cols-4 gap-6 md:grid-cols-8 lg:grid-cols-12">
-      
+  
+    // Camisa
     <div className="col-span-4 lg:col-span-7 flex flex-col justify-center p-4">
         <h2 className="text-4xl font-bold mb-4">Indumentaria</h2>
         <p className="mb-4">
-        PANTALON DE GABARDINA 8 0Z.
-        Confeccionados con triple costura - con boton y cierre para una mayor durabilidad pensados para soportar los esfuerzos diarios.
+        CAMISA DE GABARDINA.
+        Camisas gabaridna rigida de 6 oz con mangas reforzadas 2 bolsillos delanteros. reforzadas en las zonas de mayor exigencia.
         </p>
       </div>  
-      
-      
+            
       <div className="col-span-4 lg:col-span-5 p-4">
         <img
           className="object-cover h-60 w-full rounded-lg"
@@ -53,7 +57,46 @@ export default function Indumentaria() {
           alt="Imagen"
         />
       </div>
+            
+// Pantalon
+    <div className="col-span-4 lg:col-span-7 flex flex-col justify-center p-4">
+        <h2 className="text-4xl font-bold mb-4">Indumentaria</h2>
+        <p className="mb-4">
+        PANTALON DE GABARDINA.
+        Pantalon de gabardina 8 oz con boton y cierre, triple constura, 2 bolsillos laterales con forreria de primera calidad y un bolsillo ojal resistente.
+        </p>
+      </div>  
+      <div className="col-span-4 lg:col-span-5 p-4">
+        <img
+          className="object-cover h-60 w-full rounded-lg"
+          src={pantalonazulino}
+          alt="Imagen"
+        />
+      </div>
+    
+      <div className="col-span-4 max-w-5xl md:col-span-12 bg-gradient-to-r from-slate-200 to-slate-50 p-4 rounded-lg shadow-lg">        <img
+          className="mx-auto h-96 w-full md:rounded-l-lg"
+          src={pantaloncolores}
+          alt="Imagen"
+        />
+      </div>
 
+      <div className="col-span-4 lg:col-span-5 bg-gradient-to-r from-slate-200 to-slate-50 p-4 rounded-lg shadow-lg">
+        <img
+          className="object-cover h-60 w-full md:rounded-l-lg"
+          src={pantalonbeige}
+          alt="Imagen"
+        />
+      </div>
+
+      <div className="col-span-4 lg:col-span-7 bg-gradient-to-r from-slate-200 to-slate-50 p-4 rounded-lg shadow-lg">
+        <img
+          className="object-cover h-60 w-full md:rounded-l-lg"
+          src={pantalonmarino}
+          alt="Imagen"
+        />
+      </div>
+            
     </div>
   </div>  
   {/* Agregar Numero de Telefono */}
