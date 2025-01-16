c03976f64732b8e980fd28b3e935933e05c83dac
Diego Iacub <105656485+iacubdiego@users.noreply.github.com>
2025-01-16 13:07:24 -0300

    Update index.js

--- src/pages/Calzado/index.js
+++ src/pages/Calzado/index.js
@@ -3,68 +3,64 @@ import WhatsAppIcon from '../../components/WhatsAppIcon';
 
 import botin from "../../images/botin de seguridad.jpg";
 import botinfondo from "../../images/botin de seguridad fondo.jpg";
+import botaslluvia from "../../images/Botas.jpg";
+
 import botincerca from "../../images/botin seguridad cerca.jpg";
 import botinsuela from "../../images/botin suela.jpg";
 
-
 export default function Calzado() {
   return (
-    <>
+  <>
    <div className="container mx-auto ">
+    <h2 className="p-4 mx-auto col-span-4 md:col-span-8 lg:col-span-12 text-6xl font-bold m-4">CALZADO</h2>    
+    <div className="relative p-4 grid grid-cols-4 gap-6 md:grid-cols-8 lg:grid-cols-12">      
     
-    <div className="relative p-4 grid grid-cols-4 gap-6 md:grid-cols-8 lg:grid-cols-12">
-      
-    <div className="col-span-4 lg:col-span-7 flex flex-col justify-center p-4">
-        <h2 className="text-4xl font-bold mb-4">Botas y Zapatos para el trabajo</h2>
+      <div className="col-span-4 md:col-span-8 lg:col-span-12 p-4 m-4">
+        <h2 className="text-4xl font-bold mb-4">Botines</h2>
         <p className="mb-4">
           Botas de PVC con punta de acero de la marca Proforce        
         </p>
       </div>  
-      
-      
-      <div className="col-span-4 lg:col-span-5 p-4">
+
+      <div className="relative w-full h-[24rem] col-span-4 lg:col-span-6 bg-gradient-to-r from-slate-200 to-slate-50 p-4 rounded-lg shadow-lg">
         <img
-          className="object-cover h-60 w-full rounded-lg"
+          className="object-contain w-full h-full"
           src={botin}
           alt="Imagen"
         />
       </div>
-    
-      <div className="col-span-4 max-w-5xl md:col-span-12 bg-gradient-to-r from-slate-200 to-slate-50 p-4 rounded-lg shadow-lg">         <img
-          className="mx-auto h-96 w-full md:rounded-l-lg"
+      <div className="relative w-full h-[24rem] col-span-4 lg:col-span-6 bg-gradient-to-r from-slate-200 to-slate-50 p-4 rounded-lg shadow-lg">
+        <img
+          className="object-contain w-full h-full"
           src={botinfondo}
           alt="Imagen"
         />
       </div>
 
-      <div className="col-span-4 lg:col-span-5 bg-gradient-to-r from-slate-200 to-slate-50 p-4 rounded-lg shadow-lg">
+      
+      <div className="col-span-4 md:col-span-8 lg:col-span-12 p-4 m-4">
+        <h2 className="text-4xl font-bold mb-4">Botas</h2>
+        <p className="mb-4">
+          Botas de lluvia pvc con puntera de acero        
+        </p>
+      </div>  
+
+      <div className="relative w-full h-[24rem] col-span-4 lg:col-span-6 bg-gradient-to-r from-slate-200 to-slate-50 p-4 rounded-lg shadow-lg">
         <img
-          className="object-cover h-60 w-full md:rounded-l-lg"
-          src={botincerca}
+          className="object-contain w-full h-full"
+          src={botaslluvia}
           alt="Imagen"
         />
       </div>
 
-      <div className="col-span-4 lg:col-span-7 bg-gradient-to-r from-slate-200 to-slate-50 p-4 rounded-lg shadow-lg">
-        <img
-          className="object-cover h-60 w-full md:rounded-l-lg"
-          src={botinsuela}
-          alt="Imagen"
-        />
-      </div>
-
+            
     </div>
-
-    <div>
-
-    </div>
-
   </div>  
 
   {/* Agregar Numero de Telefono */}
   <WhatsAppIcon phoneNumber="NUMERO_DE_TELEFONO" />
 
-  </>
+</>
   );
 }
 
