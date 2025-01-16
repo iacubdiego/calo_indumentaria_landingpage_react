9c8c1d374ef22bb185e4c16faa2d6ba5c70a4a06
Diego Iacub <105656485+iacubdiego@users.noreply.github.com>
2025-01-16 14:01:52 -0300

    Update index.js

--- src/pages/Calzado/index.js
+++ src/pages/Calzado/index.js
@@ -12,7 +12,7 @@ export default function Calzado() {
   return (
   <>
    <div className="container mx-auto ">
-    <h2 className="p-4 mx-auto col-span-4 md:col-span-8 lg:col-span-12 text-6xl font-bold m-4">CALZADO</h2>    
+    <h2 className="p-4 mx-auto col-span-4 md:col-span-8 lg:col-span-12 text-4xl font-bold m-4">CALZADO</h2>    
     <div className="relative p-4 grid grid-cols-4 gap-6 md:grid-cols-8 lg:grid-cols-12">      
     
       <div className="col-span-4 md:col-span-8 lg:col-span-12 p-4 m-4">
