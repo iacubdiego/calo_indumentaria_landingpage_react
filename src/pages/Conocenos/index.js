b52948bd5083c71e14aa3c752afbadf4436a7949
Diego Iacub <105656485+iacubdiego@users.noreply.github.com>
2025-01-20 16:15:56 -0300

    Update index.js

--- src/pages/Conocenos/index.js
+++ src/pages/Conocenos/index.js
@@ -7,77 +7,78 @@ const Nosotros = () => {
       <div className="relative bg-blue-900 w-screen flex flex-col">
       
 
-          {/*      <!-- Component: Elevated centered simple base sized testimonial --> */}
-          <div className="w-3/4 mx-14 mt-4 max-w-screen-xl overflow-hidden text-center bg-white bg-opacity-75 backdrop-blur-sm rounded-lg shadow-2xl text-slate-500 shadow-slate-200">
+          {/*      <!-- Quienes Somos --> */}
+          <div className="w-3/4 mx-auto mt-4 max-w-screen-xl overflow-hidden text-center bg-white bg-opacity-75 backdrop-blur-sm rounded-lg shadow-2xl text-slate-500 shadow-slate-200">
             <div className="relative p-6 bg-white">
+              <h2 className="text-3xl font-bold mb-4 text-black">Quienes somos</h2>
               <figure className="relative z-10">
                 <blockquote className="p-6 text-xl leading-relaxed">
-                  <p>
-                  En CALO nos especializamos en la fabricación y comercialización de indumentaria laboral y elementos de protección personal. (Abastecemos) Nos dirigimos a una amplia gama de clientes, desde pymes y mayoristas hasta particulares, ofreciendo productos de alta calidad que combinan precio y durabilidad, que permitan equipar a los trabajadores de cada sector donde se forja la Industria Nacional.
-                  </p>
+                    <p>
+                    Bienvenidos a CALO indumentaria laboral , somos fabricantes y distribuidores de ropa de trabajo y elementos de seguridad en el rubro de la construcción y la Industria Nacional. Con más de 10 años de experiencia y compromiso con la excelencia, nos hemos consolidado como proveedores confiables para nuestros clientes.
+                    </p>
                 </blockquote>
                 <figcaption className="flex justify-center items-center gap-4 p-6 pt-2 text-xl text-white-500">
                 </figcaption>
               </figure>
             </div>
           </div>
-          {/*      <!-- End Elevated centered simple base sized testimonial --> */}
+          {/*      <!-- End Quienes Somos --> */}
 
-          <div className='flex flex-wrap w-screen'>
-
-{/* <!-- Component: Elevated centered simple base sized testimonial --> */}
-<div className='w-full md:w-1/2 p-2'>
-  <div className="p-2 m-6 overflow-hidden text-center bg-gradient-to-b from-orange-400 to-orange-200 rounded-lg text-slate-500">
-    <div className="relative p-2">
-      <h2 className="text-3xl font-bold mb-4 text-black">Nuestros Valores</h2>
-      <figure className="relative z-10">
-        <blockquote className="p-6 text-xl leading-relaxed">
-          <p className="p-2 text-left">- Calidad y compromiso en nuestros productos y servicios.</p>
-          <p className="p-2 text-left">- Atención personalizada y soporte técnico especializado.</p>
-          <p className="p-2 text-left">- Innovación constante para satisfacer las necesidades del mercado.</p>
-        </blockquote>
-      </figure>
-    </div>
-  </div>
-</div>
-{/* <!-- End Elevated centered simple base sized testimonial --> */}
-{/* <!-- Component: Elevated centered simple base sized testimonial --> */}
-<div className='w-full md:w-1/2 p-2'>
-  <div className="p-2 m-6 overflow-hidden text-center bg-gradient-to-b from-orange-200 to-orange-400 rounded-lg text-slate-500">
-    <div className="relative p-6">
-    <h2 className="text-3xl font-bold mb-4 text-black">Nuestros Productos y Servicios</h2>
-      <figure className="relative z-10">
-        <blockquote className="p-2 text-xl leading-relaxed">
-          <p className="p-2 text-left">- Ropa de trabajo resistente y calidad asegurada</p>
-          <p className="p-2 text-left">- Elementos de seguridad</p>
-          <p className="p-2 text-left">- Calzado industrial y de seguridad</p>
-          <p className="p-2 text-left">- Asesoramiento técnico y presupuestos en 24hs</p>
-          <p className="p-2 text-left">- Envios a todo el País</p>
-        </blockquote>
-      </figure>
-    </div>
-  </div>
-</div>
-{/* <!-- End Elevated centered simple base sized testimonial --> */}
-        </div>
-
-          {/*      <!-- Component: Elevated centered simple base sized testimonial --> */}
-          <div className="w-4/5 max-w-screen-md p-2 ml-auto mx-8 mb-4 overflow-hidden text-center bg-white bg-opacity-75 backdrop-blur-sm rounded-lg shadow-2xl text-slate-500 shadow-slate-200">
+          {/*      <!-- Nuestra Misión --> */}
+          <div className="w-3/4 mx-auto mt-4 max-w-screen-xl overflow-hidden text-center bg-white bg-opacity-75 backdrop-blur-sm rounded-lg shadow-2xl text-slate-500 shadow-slate-200">
             <div className="relative p-6 bg-white">
-
-            <h2 className="text-3xl font-bold mb-4 text-black">Nuestra Misión</h2>
-              <figure className="relative z-10">
-                <blockquote className="p-2 text-xl leading-relaxed">
-                  <p>
-Nuestra misión es proporcionar soluciones integrales de protección personal y ropa de trabajo, garantizando la seguridad, calidad en la indumentaria de los trabajadores en sus entornos laborables. Nos esforzamos por entender las necesidades específicas de cada cliente y ofrecer productos innovadores que superen sus expectativas.
-                  </p>
-                </blockquote>              </figure>
+                <h2 className="text-3xl font-bold mb-4 text-black">Nuestra Misión</h2>
+                <figure className="relative z-10">
+                  <blockquote className="p-2 text-xl leading-relaxed">
+                    <p>
+                     Nuestra misión es proporcionar soluciones integrales de protección personal y ropa de trabajo, garantizando la seguridad, calidad en la indumentaria de los trabajadores en sus entornos laborables. Nos esforzamos por entender las necesidades específicas de cada cliente y ofrecer productos innovadores que superen sus expectativas.
+                    </p>
+                </blockquote>              
+              </figure>
             </div>
           </div>
-          {/*      <!-- End Elevated centered simple base sized testimonial --> */}
+          {/*      <!-- End Nuestra Misión --> */}
       </div>
+    
+          <div className='flex flex-wrap w-screen'>
 
-      {/* Agregar Numero de Telefono */}
+        {/* <!-- Nuestros Valore --> */}
+        <div className='w-full md:w-1/2 p-2'>
+          <div className="p-2 m-6 overflow-hidden text-center bg-gradient-to-b from-orange-400 to-orange-200 rounded-lg text-slate-500">
+            <div className="relative p-2">
+              <h2 className="text-3xl font-bold mb-4 text-black">Nuestros Valores</h2>
+              <figure className="relative z-10">
+                <blockquote className="p-6 text-xl leading-relaxed">
+                  <p className="p-2 text-left">- Calidad y compromiso en nuestros productos y servicios.</p>
+                  <p className="p-2 text-left">- Atención personalizada y soporte técnico especializado.</p>
+                  <p className="p-2 text-left">- Innovación constante para satisfacer las necesidades del mercado.</p>
+                </blockquote>
+              </figure>
+            </div>
+          </div>
+        </div>
+        {/* <!-- End Nuestros Valore --> */}
+
+        {/* <!-- Nuestros Productos y Servicios --> */}
+        <div className='w-full md:w-1/2 p-2'>
+          <div className="p-2 m-6 overflow-hidden text-center bg-gradient-to-b from-orange-200 to-orange-400 rounded-lg text-slate-500">
+            <div className="relative p-6">
+            <h2 className="text-3xl font-bold mb-4 text-black">Nuestros Productos y Servicios</h2>
+              <figure className="relative z-10">
+                <blockquote className="p-2 text-xl leading-relaxed">
+                  <p className="p-2 text-left">- Ropa de trabajo resistente y calidad asegurada</p>
+                  <p className="p-2 text-left">- Elementos de seguridad</p>
+                  <p className="p-2 text-left">- Calzado industrial y de seguridad</p>
+                  <p className="p-2 text-left">- Asesoramiento técnico y presupuestos en 24hs</p>
+                  <p className="p-2 text-left">- Envios a todo el País</p>
+                </blockquote>
+              </figure>
+            </div>
+          </div>
+        </div>
+        {/* <!-- End Nuestros Productos y Servicios --> */}
+        </div>
+
       <WhatsAppIcon phoneNumber="541156379430" />
   </>
   )
